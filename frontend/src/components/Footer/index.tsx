e4c31f7316cbde2eadee2a85ee00782e77469012
matheusned15 <matheusned15@gmail.com>
2021-05-05 17:10:42 -0300

    Static charts

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/matheusned15?tab=overview&from=2021-03-01&to=2021-03-17" target="_blank" rel="noreferrer">Matheus Ned</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
